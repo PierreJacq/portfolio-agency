d800676d3f3d4101bb85ddb9583896f64663b073
¨Pierre Jacquemin <pierre.jacquemin.ams@gmail.com>
2022-01-26 18:33:50 +0100

    Travail animation navbar

---
+++ index.js
@@ -0,0 +1,10 @@
+// Gestion de la nav-bar
+
+const navbar = document.querySelector('.nav-fixed');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
