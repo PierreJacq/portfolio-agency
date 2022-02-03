ef4dbc2b351a5ebab7179d8451507ef0a5f983be
Pierre Jacquemin <pierre.jacquemin.ams@gmail.com>
2022-02-03 19:16:26 +0100

    Slider - ajout animation défilement - animation bulles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Gestion de la nav-bar
 
 const navbar = document.querySelector('#nav-on-top');
+
 window.onscroll = () => {
     if (window.scrollY > 50) {
         navbar.classList.add('nav-scrolled');
@@ -8,3 +9,59 @@ window.onscroll = () => {
         navbar.classList.remove('nav-scrolled');
     }
 };
+
+//--------------------------------------
+// Animation de l'image en background
+//--------------------------------------
+
+//Valeurs initiales et sélecteurs fixes
+const arrayBackground = [
+    "/public/img/profil.jpg", 
+    "/public/img/magento.jpg", 
+    "/public/img/challenge.jpg",
+    "/public/img/success.jpg",
+    "/public/img/teamwork.jpg",
+];
+var indexSlider = 0;
+
+const backgroundCible = document.getElementById("slider--background");
+const tousSlides = document.getElementsByClassName("slider--slide");
+const toutesBullet = document.querySelectorAll(".fas.fa-circle");
+
+for (var mi = 0; mi < toutesBullet.length; mi++) {
+    toutesBullet[mi].addEventListener("click", () => {
+        indexSlider = [mi];
+        console.log(indexSlider)
+    });
+};
+
+
+//Slide initial
+var slideCible = document.getElementById("slide" + indexSlider + "");
+slideCible.style.display = "block";
+
+//Fonction pour switch
+const changeBackground = () => {
+    //mise à jour de l'index
+    indexSlider = indexSlider + 1;
+    if (indexSlider == arrayBackground.length) {
+        indexSlider = 0;
+    }
+
+    // Mise à jour texte correct
+    for (i = 0; i < tousSlides.length; i++) {
+        tousSlides[i].style.display = "none"
+    };
+    backgroundCible.style.backgroundImage = "url(" + arrayBackground[indexSlider] + ")";
+    slideCible = document.getElementById("slide" + indexSlider + "");
+    slideCible.style.display = "block";
+    
+    //Mise à jour bullets
+    for (i = 0; i < toutesBullet.length; i++) {
+        toutesBullet[i].style.color = "#808080";
+    };
+    var bulletCible = document.getElementById("circle" + indexSlider + "");
+    bulletCible.style.color = "#3950FF";
+
+};
+setInterval(changeBackground, 5000);
